d5c170fd47f5378e4eae67b7b5bf9809b21b21ce
Jose Mosquera <jojumofu@gmail.com>
2023-07-07 20:11:13 -0500

    feat: add PyPDF2 to read .pdf and process with gtts

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+import PyPDF2
 from gtts import gTTS
 import os
+
+
+def read_pdf(filename):
+    with open(filename, "rb") as file:
+        reader = PyPDF2.PdfReader(file)
+        num_pages = len(reader.pages)
+
+        pages = []
+        for page in range(num_pages):
+            current_page = reader.pages[page]
+            text = current_page.extract_text()
+            pages.append(text)
+
+        return pages
 
 
 def text_to_speech(text, filename):
@@ -7,9 +22,28 @@ def text_to_speech(text, filename):
     tts.save(filename)
 
 
-if __name__ == "__main__":
-    text = "Hola mundo"  # Replace with your desired text
-    filename = "output.mp3"  # Output file name
+def main():
+    filename = "test.pdf"  # Replace with your PDF file
+    output_folder = "output"  # Output folder for generated audio files
 
-    text_to_speech(text, filename)
-    os.system("mpg123 " + filename)
+    # Create output folder if it doesn't exist
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    # Read the PDF
+    pages = read_pdf(filename)
+
+    # Convert each page to speech
+    for i, page_content in enumerate(pages):
+        # Generate filename for the audio file
+        output_filename = os.path.join(output_folder, f"output_{i + 1}.mp3")
+
+        # Convert text to speech and save as an audio file
+        text_to_speech(page_content, output_filename)
+
+        # Play the audio file
+        os.system("mpg123 " + output_filename)
+
+
+if __name__ == "__main__":
+    main()
